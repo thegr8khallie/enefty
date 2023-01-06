b66e7f263ab99e2019ea32908dd3a28bee06fd9f
Victor Anyadioha <talk2okwundu@gmail.com>
2023-01-06 07:14:02 +0100

    Style: Added simple global styling and resets

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,45 @@
+import { Plus_Jakarta_Sans as Pjsans } from "@next/font/google";
+
+const pjsans = Pjsans({ subsets: ["latin"] });
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <style jsx global>{`
+        *,
+        *::before,
+        *::after {
+          padding: 0;
+          margin: 0;
+          box-sizing: border-box;
+        }
+        ul {
+          list-style: none;
+        }
+        a {
+          text-decoration: none;
+        }
+        html {
+          font-size: 10px;
+          font-family: ${pjsans.style.fontFamily};
+        }
+        body {
+          font-size: 1.6rem;
+        }
+        img {
+          width: 100%;
+          height: 100%;
+          object-fit: cover;
+          object-position: center;
+        }
+        h1 {
+          font-weight: 700;
+        }
+        p {
+          font-weight: 500;
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 }
