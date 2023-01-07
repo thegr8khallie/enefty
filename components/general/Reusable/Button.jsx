0928bea7adbc2686c0c9ef06d4f4b588f54853e9
Victor Anyadioha <talk2okwundu@gmail.com>
2023-01-07 07:55:07 +0100

    Feat: Initialised reusable button component

---
+++ components/general/Reusable/Button.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+    padding: 1.5rem 2.5rem;
+    background-color: var(--charleston-green);
+    color: var(--silver-sand);
+    border: none;
+    border-radius: .8rem;
+`
