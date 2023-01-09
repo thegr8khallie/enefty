a9d60f009f2f919402f7de7827c95358234de820
Victor Anyadioha <talk2okwundu@gmail.com>
2023-01-09 07:45:34 +0100

    Feat: Created Hero component

--- components/index/components/Hero.jsx
+++ components/index/components/Hero.jsx
@@ -1,9 +1,19 @@
 import { StyledHero } from "../styled/Hero.styled";
+import { Button } from '../../general/Reusable'
 
 export const Hero = () => {
     return (
         <StyledHero>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam magnam labore hic reiciendis molestias temporibus repellendus aperiam est consectetur. Fugiat quam, nam veritatis dolorem soluta maxime excepturi odit cumque recusandae eius alias nesciunt assumenda rerum? Atque eius vitae fuga ratione excepturi expedita asperiores, cum minima repellat aut? Rem eaque similique quod corrupti praesentium ratione esse obcaecati neque iste enim quia consequatur vitae, amet veniam iure fugiat debitis possimus adipisci, voluptas non eius sunt, reprehenderit quas. Amet laborum, rerum consequuntur, officiis ad obcaecati quibusdam blanditiis ducimus vel eveniet illo assumenda a velit quasi adipisci at itaque. Rem deleniti fuga nostrum accusantium.
+            <h1>
+                Create tamper proof <span>NFTs</span> for your digital products on the Algorand blockchain.
+            </h1>
+            <p>
+                Don&#39;t know how to work with NFTs? All you have to do is a single upload and we will do all the heavy lifting for you!
+            </p>
+            <div className="btn-container">
+                <Button gs>Get Started</Button>
+                <Button create>Create NFTs</Button>
+            </div>
         </StyledHero>
     )
 }
