ff21e6411bb1d35fc6a235173da815fe2bd4e9a5
Victor Anyadioha <talk2okwundu@gmail.com>
2023-01-09 07:04:15 +0100

    Test

--- components/index/components/Hero.jsx
+++ components/index/components/Hero.jsx
@@ -3,7 +3,7 @@ import { StyledHero } from "../styled/Hero.styled";
 export const Hero = () => {
     return (
         <StyledHero>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla expedita cumque provident ex tempore veritatis perferendis sed ea dolores, voluptatibus totam ipsam id earum assumenda harum nobis voluptatum quasi et culpa quis omnis deleniti modi porro? Quas libero voluptates similique, laborum doloribus, mollitia vitae possimus, voluptate veniam sit veritatis nobis.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam magnam labore hic reiciendis molestias temporibus repellendus aperiam est consectetur. Fugiat quam, nam veritatis dolorem soluta maxime excepturi odit cumque recusandae eius alias nesciunt assumenda rerum? Atque eius vitae fuga ratione excepturi expedita asperiores, cum minima repellat aut? Rem eaque similique quod corrupti praesentium ratione esse obcaecati neque iste enim quia consequatur vitae, amet veniam iure fugiat debitis possimus adipisci, voluptas non eius sunt, reprehenderit quas. Amet laborum, rerum consequuntur, officiis ad obcaecati quibusdam blanditiis ducimus vel eveniet illo assumenda a velit quasi adipisci at itaque. Rem deleniti fuga nostrum accusantium.
         </StyledHero>
     )
 }
