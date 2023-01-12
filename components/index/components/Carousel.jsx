fdd15212954373e6b4c829b8ed2a1d097321b8cc
Victor Anyadioha <talk2okwundu@gmail.com>
2023-01-12 13:45:08 +0100

    Feat: Basic Carousel markup  done

--- components/index/components/Carousel.jsx
+++ components/index/components/Carousel.jsx
@@ -2,6 +2,21 @@ import { StyledCarousel } from "../styled/Carousel.styled";
 
 export const Carousel = () => {
     return (
-        <StyledCarousel></StyledCarousel>
+        <StyledCarousel>
+            <a href="http://" target="_blank" rel="noopener noreferrer">
+                <div className="img-container"></div>
+            </a>
+            <div className="flickity-container">
+                <div className="flickity">
+                    <div className="flickity-filler"></div>
+                </div>
+            </div>
+            <div className="seek-container">
+                <button></button>
+                <div />
+                <button></button>
+            </div>
+            <p>1<span>/3</span></p>
+        </StyledCarousel>
     )
 }
