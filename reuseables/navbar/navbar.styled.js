3c2a45301376abce4412a160c3a542f19df8e1da
Victor Anyadioha <victoranyadioha@gmail.com>
2023-01-24 03:04:18 +0100

    Style: Style Changes to navbar due to connect wallet feat impleamentation

--- reuseables/navbar/navbar.styled.js
+++ reuseables/navbar/navbar.styled.js
@@ -5,11 +5,23 @@ const StyledNav = styled.nav`
   padding: 2rem;
   display: flex;
   box-shadow: 0 1rem 0.7rem 0 #121128ea;
-  div {
+  > div {
     margin-left: auto;
+    display: flex;
+    gap: 2rem;
+    > div {
+      width: 15rem;
+      padding: 1rem;
+      background-color: #111;
+      overflow: hidden;
+      span {
+        width: 100%;
+      }
+    }
     button {
       padding: 1rem;
-      background-color: purple;
+      background-color: #3f48cc;
+      border: none;
     }
   }
 `;
