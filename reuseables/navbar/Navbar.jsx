3d6421a1ad4ae185e25f87dc5009619bdb57fe8f
Victor Anyadioha <victoranyadioha@gmail.com>
2023-01-24 03:03:44 +0100

    Feat: Added Connect wallet Functionality

--- reuseables/navbar/Navbar.jsx
+++ reuseables/navbar/Navbar.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import StyledNav from './navbar.styled'
+import MyAlgoConnect from '@randlabs/myalgo-connect'
+import algosdk from 'algosdk'
 
 export const Navbar = () => {
+    const account = useRef();
+    const [addr, setAddr] = useState('')
+
+    const handleWalletConnect = async () => {
+        try {
+            const myAlgoWallet = new MyAlgoConnect();
+            account.current = await myAlgoWallet.connect();
+            setAddr(account.current[0].address);
+        } catch (error) {
+            console.error(error)
+        }
+    }
     return (
         <StyledNav>
             <div>
-                <button>Connect Wallet</button>
+                {addr !== "" && <div><span>{addr}</span></div>}
+                <button onClick={handleWalletConnect}>{addr === "" ? "Connect myAlgo Wallet" : 'Disconnect'}</button>
             </div>
         </StyledNav>
     )
