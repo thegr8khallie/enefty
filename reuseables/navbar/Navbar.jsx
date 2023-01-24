a80295f76c0faad5cdd1b581b57eacca5b70d219
Victor Anyadioha <victoranyadioha@gmail.com>
2023-01-24 04:16:48 +0100

    Feat: Made Connected address to persist on refresh.

--- reuseables/navbar/Navbar.jsx
+++ reuseables/navbar/Navbar.jsx
@@ -1,16 +1,21 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import StyledNav from './navbar.styled'
 import MyAlgoConnect from '@randlabs/myalgo-connect'
-import algosdk from 'algosdk'
 
 export const Navbar = () => {
     const account = useRef();
     const [addr, setAddr] = useState('')
+    useEffect(() => {
+        if (localStorage.getItem('address')) {
+            setAddr(JSON.parse(localStorage.getItem('address')))
+        }
+    }, [])
 
     const handleWalletConnect = async () => {
         try {
             const myAlgoWallet = new MyAlgoConnect();
             account.current = await myAlgoWallet.connect();
+            localStorage.setItem('address', account.current[0].address)
             setAddr(account.current[0].address);
         } catch (error) {
             console.error(error)
