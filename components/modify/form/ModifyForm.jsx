952ae4cd57d05c0cf35383fd88d0dfc7b7cb30d7
Victor Anyadioha <victoranyadioha@gmail.com>
2023-01-25 03:54:34 +0100

    Refactor: Slight Changes to structure

--- components/modify/form/ModifyForm.jsx
+++ components/modify/form/ModifyForm.jsx
@@ -1,25 +1,15 @@
-import { useState } from "react";
-import DestroyForm from "../destroyform/DestroyForm";
-import EditForm from "../editform/EditForm";
 import StyledModifyForm from "./modifyForm.styled";
 
 export const ModifyForm = () => {
-    const [formState, setFormState] = useState('');
 
-    const handleChange = (e) => {
-        const selectBox = e.target;
-        var selectedValue = selectBox.options[selectBox.selectedIndex].value;
-        setFormState(selectedValue);
-    }
+
     return (
         <StyledModifyForm>
             <h1>Modify Your already existing NFTs</h1>
-            <select onChange={handleChange}>
-                <option value="Select...">Select...</option>
-                <option value="Edit">Edit</option>
-                <option value="Destroy">Destroy</option>
-            </select>
-            {formState === 'Edit' ? <EditForm /> : formState === 'Destroy' ? <DestroyForm /> : null}
+            <label htmlFor="assetID">AssetID</label>
+            <input type="text" id="assetID" placeholder="Enter AssetID" />
+            <label htmlFor="description">Description</label>
+            <input type="text" id="description" placeholder="Edit Asset description" />
         </StyledModifyForm>
     )
 }
--- components/modify/form/modifyForm.styled.js
+++ components/modify/form/modifyForm.styled.js
@@ -5,13 +5,24 @@ const StyledModifyForm = styled.form`
   h1 {
     margin-bottom: 2rem;
   }
-  select {
-    padding: 1rem;
+  label,
+  input {
+    display: block;
     margin-bottom: 2rem;
+  }
+  input {
+    width: 100vw;
+    max-width: 50rem;
+    padding: 1rem;
+    border: 1px solid;
     &:focus {
-      border: none;
       outline: none;
+      border: 0.1rem solid #84caff;
+      box-shadow: 0.1rem 0.2rem #1018280d, 0 0 0 0.4rem #d1e9ff;
     }
+  }
+  label {
+    font-size: 2rem;
   }
 `;
 
