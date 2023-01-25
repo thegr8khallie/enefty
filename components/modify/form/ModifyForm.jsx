778acd095f0906e8113eb3e2e32b44d6724b1241
Victor Anyadioha <victoranyadioha@gmail.com>
2023-01-25 03:03:55 +0100

    Feat: Set up basic select functionality for Form

--- components/modify/form/ModifyForm.jsx
+++ components/modify/form/ModifyForm.jsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
 import StyledModifyForm from "./modifyForm.styled";
 
 export const ModifyForm = () => {
+    const [formState, setFormState] = useState('');
     return (
         <StyledModifyForm>
-            <h1>Hello from modify form</h1>
+            <h1>Modify Your already existing NFTs</h1>
+            <select>
+                <option value="Select...">Select...</option>
+                <option value="Edit" onClick={() => setFormState('Edit')}>Edit</option>
+                <option value="Destroy" onClick={() => setFormState('Destroy')}>Destroy</option>
+            </select>
         </StyledModifyForm>
     )
 }
--- components/modify/form/modifyForm.styled.js
+++ components/modify/form/modifyForm.styled.js
@@ -1,5 +1,13 @@
 import styled from "styled-components";
 
-const StyledModifyForm = styled.form``;
+const StyledModifyForm = styled.form`
+  padding: 4rem;
+  select {
+    padding: 1rem;
+    option {
+      padding: 1rem;
+    }
+  }
+`;
 
 export default StyledModifyForm;
