c642690c2c10a0ce43b4246d83d3a64351c8bb03
Victor Anyadioha <victoranyadioha@gmail.com>
2023-01-25 02:07:16 +0100

    Feat: Edits made to Head tag of various pages for SEO

---
+++ pages/modify.jsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Modify() {
+    return (
+        <>
+            <Head>
+                <title>Modify</title>
+                <meta name="description" content="Edit Existing NFT Metadata" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+        </>
+    );
+}
