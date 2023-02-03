e299e9dccfdb4389cefe9031db1118a5f7956117
Victor Anyadioha <victoranyadioha@gmail.com>
2023-02-03 16:08:46 +0100

    Chore: UseAsync hook cleanup

--- hooks/useAsync.jsx
+++ hooks/useAsync.jsx
@@ -4,10 +4,6 @@ export const useAsync = (asyncFunction, immediate = true) => {
     const [status, setStatus] = useState("idle");
     const [value, setValue] = useState(null);
     const [error, setError] = useState(null);
-    // The execute function wraps asyncFunction and
-    // handles setting state for pending, value, and error.
-    // useCallback ensures the below useEffect is not called
-    // on every render, but only if asyncFunction changes.
     const execute = useCallback(() => {
         setStatus("pending");
         setValue(null);
@@ -22,9 +18,6 @@ export const useAsync = (asyncFunction, immediate = true) => {
                 setStatus("error");
             });
     }, [asyncFunction]);
-    // Call execute if we want to fire it right away.
-    // Otherwise execute can be called later, such as
-    // in an onClick handler.
     useEffect(() => {
         if (immediate) {
             execute();
